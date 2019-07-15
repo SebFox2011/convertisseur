622d3be6e6f140eb5ba37eedeb609c2b2a35b079
SebFox2011 <sebastien.philippe5@gmail.com>
2019-07-15 16:36:09 +0200

    ajout des monnaies dans le select avec eur en plus

--- src/component/currency/CurrencyConverter.js
+++ src/component/currency/CurrencyConverter.js
@@ -4,7 +4,7 @@ class CurrencyConverter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            currencies: []
         }
     }
 //chargement du composant
@@ -12,20 +12,27 @@ class CurrencyConverter extends Component {
         fetch('https://api.exchangeratesapi.io/latest')
             .then(response => response.json ())
             .then(data =>{
-               console.log(data);
+               let currencies = Object.keys(data.rates);
+               currencies.push(data.base);
+               this.setState({currencies:currencies.sort()});
             });
     }
 
 
     render() {
+
+        const currencyOptions = this.state.currencies.map(
+            currency => <option key={currency} value={currency}>{currency}</option>
+        );
+
         return (
             <form>
                 <input type="number"/>
                 <select>
-
+                    {currencyOptions}
                 </select>
                 <select>
-
+                    {currencyOptions}
                 </select>
                 <input type="submit"/>
             </form>
