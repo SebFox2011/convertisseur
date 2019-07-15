1882ba0e55535d52983c1976ca56d971300009fc
SebFox2011 <sebastien.philippe5@gmail.com>
2019-07-15 16:08:28 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CurrencyConverter from "./components/currency/CurrencyConverter";
+
+function App() {
+  return (
+      <div className="App">
+        <CurrencyConverter/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/component/currency/CurrencyConverter.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class CurrencyConverter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <form>
+                <input type="number"/>
+                <select>
+
+                </select>
+                <select>
+
+                </select>
+                <input type="submit"/>
+            </form>
+        );
+    }
+}
+
+export default CurrencyConverter;
